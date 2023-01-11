230f8d66fc5cc646952ae12babb795a946da1937
Darrin Barker <darrin.barker@flatironschool.com>
2023-01-10 20:13:31 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const companyName = 'Scuber';{
+    companyName == 'Scruber';
+}
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
